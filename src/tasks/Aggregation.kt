c3f4517fe9501ca31e5cc1c5a27ee3a0a6612700
Viktor Penelski <viktor.penelski@falcon.io>
2019-10-04 02:16:38 +0300

    solution for task 1 - aggregation

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -15,4 +15,8 @@ TODO: Write aggregation code.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
 fun List<User>.aggregate(): List<User> =
-    this
+    this.groupBy { it.login }
+        .map { (login, userList) ->
+            User(login, userList.sumBy { it.contributions })
+        }
+        .sortedByDescending { it.contributions }
